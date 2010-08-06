7802d81f68c8acd2d771aa2aada4c12cfbaa523d
Jim Suchy <jsuchy@gmail.com>
2010-08-05 23:27:09 -0500

    deleting commented code

--- hello/test-hello.js
+++ hello/test-hello.js
@@ -26,8 +26,6 @@ function betterTest() {
 	request.end();
 
 	request.on('response', function (response) {
-	  // console.log('STATUS: ' + response.statusCode);
-	  // console.log('HEADERS: ' + JSON.stringify(response.headers));
 	  response.setEncoding('utf8');
 	  response.on('data', function (chunk) {
 		// Testing that the right response is sent	
