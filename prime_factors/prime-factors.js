49d1fa1d9c54cf1d660ac6fcc0ba41308ca34fe8
Jim Suchy <jsuchy@gmail.com>
2010-08-05 22:10:48 -0500

    prime factors skeleton

--- prime_factors/prime-factors.js
+++ prime_factors/prime-factors.js
@@ -2,17 +2,7 @@ exports.PrimeFactors = function () {}
 
 exports.PrimeFactors.prototype = {
 	generate: function (n) {
-		var primes = [];
-		var candidate = 2;
-		while (n > 1) {
-			while (n % candidate === 0) {
-				primes.push(candidate);
-				n /= candidate;
-			}
-			
-			candidate++;
-		}
-		return primes;
+		return [];
 	}
 	
 }
--- prime_factors/spec/test-prime-factors.js
+++ prime_factors/spec/test-prime-factors.js
@@ -11,31 +11,6 @@ vows.describe ('Prime Factors').addBatch({
 			'testOne': function (primeFactors) {
 				assert.deepEqual (primeFactors.generate(1), []);
 			},
-			'testTwo': function (primeFactors) {
-				assert.deepEqual (primeFactors.generate(2), [2]);
-			},
-			'testThree': function (primeFactors) {
-				assert.deepEqual (primeFactors.generate(3), [3]);
-			},
-			'testFour': function (primeFactors) {
-				assert.deepEqual (primeFactors.generate(4), [2, 2]);
-			},
-			'testFive': function (primeFactors) {
-				assert.deepEqual (primeFactors.generate(5), [5]);
-			},
-			'testSix': function (primeFactors) {
-				assert.deepEqual (primeFactors.generate(6), [2, 3]);
-			},
-			'testEight': function (primeFactors) {
-				assert.deepEqual (primeFactors.generate(8), [2, 2, 2]);
-			},
-			'testNine': function (primeFactors) {
-				assert.deepEqual (primeFactors.generate(9), [3, 3]);
-			},
-			'testBigNumber': function(primeFactors) {
-				var bigNumber = 2 * 2 * 2 * 3 * 7 * 13 * 23;
-				assert.deepEqual (primeFactors.generate(bigNumber), [2, 2, 2, 3, 7, 13, 23]);
-			}
 		}
 	}
 }).run();
